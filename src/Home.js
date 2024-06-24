a108c9fbff5bc716d7bfc98e206ada94e1cbe01e
ozcelikmirzabugra <ozcelik.mirzabugra@gmail.com>
2024-06-24 09:32:04 +0300

    click-event

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello' + name);
+    }
+
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => 
+                handleClickAgain('mario')
+            }>click me again</button>
+        </div>
+    );
+}
+ 
+export default Home;
